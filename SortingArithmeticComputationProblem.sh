12c18be0bd14e51d56d943d4a9bf6217df040d16
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-15 15:54:05 +0530

    ADDED:Taking Inputs from user

--- SortingArithmeticComputationProblem.sh
+++ SortingArithmeticComputationProblem.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
 
 echo "..............................Welcome to Sorting Arithmetic Computation Problem.............................."
+
+#TAKING INPUT FROM USER
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInputo
+
