b23f2aae32f307522aaf5b684569cf63901e3e03
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-15 16:01:03 +0530

    ADDED:Calculated first arithmetic computation

--- SortingArithmeticComputationProblem.sh
+++ SortingArithmeticComputationProblem.sh
@@ -5,5 +5,8 @@ echo "..............................Welcome to Sorting Arithmetic Computation Pr
 #TAKING INPUT FROM USER
 read -p "Enter first input :" firstInput
 read -p "Enter second input:" secondInput
-read -p "Enter third input :" thirdInputo
+read -p "Enter third input :" thirdInput
 
+#COMPUTE ARITHMETIC FIRST COMPUTATION
+result=`echo "$firstInput + $secondInput * $thirdInput" | bc`
+echo "Result:" $result
